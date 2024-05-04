430e610e55cc3477aa97fe7127b02fe62669a6b7
mohamed-amine <mohamed.dahri@hayolabs.com>
2024-05-04 14:54:08 +0200

    server ws

---
+++ server/server.ts
@@ -0,0 +1,31 @@
+import WebSocket from "ws";
+
+const CLIENT_CONNECTED_IN_SAME_TIME = 2
+const clients: WebSocket[] = []
+
+const wss = new WebSocket.Server({ port: 8000 });
+
+wss.on('listening', () => {
+  console.log('listening')
+})
+
+wss.on('connection', (ws: WebSocket) => {
+  if(clients.length === CLIENT_CONNECTED_IN_SAME_TIME) {
+    ws.close(1000, 'Clients limit reached')
+    return
+  }
+
+  clients.push(ws)
+  console.log('New client connected');
+
+  ws.on('message', (ws: WebSocket, message: any) => {
+    const clientToTransfer = clients.find(c => c !== ws)
+    clientToTransfer?.send(message)
+  });
+
+  ws.on('close', (ws: WebSocket) => {
+    console.log('Client disconnected');
+    const index = clients.indexOf(ws);
+    if (index !== -1) clients.splice(index, 1);
+  });
+});
